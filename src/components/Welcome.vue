aef220c91fbfb50ef8356c524dc78c4657dc8111
zhangjie <hardzj@163.com>
2020-08-05 15:48:08 +0800

    上传个人心得

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,6 +1,11 @@
 <template>
   <div >
       <h3>Welcome to here!</h3>
+      <!-- 学习心得 -->
+      <div>
+        敲了半个多月，熟悉了路由懒加载，echarts报表，nprogress进度插件，lodash深拷贝，async/await语法，字符串的padstart（）方法，
+        elementui组件的大体熟练，富文本编译器插件的使用
+      </div>
   </div>
 </template>
 
